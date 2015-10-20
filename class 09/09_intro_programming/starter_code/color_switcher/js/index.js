3e2d50b6bfa8ddd138b6dcbbde2a0015555fc0c8
terrieyoung <tyoung@jbsinternational.com>
2015-10-19 20:22:22 -0400

    class 09 exercise
    misc exercise files

--- class 09/09_intro_programming/starter_code/color_switcher/js/index.js
+++ class 09/09_intro_programming/starter_code/color_switcher/js/index.js
@@ -1,16 +1,23 @@
 var grayColor = 'gray';
 var whiteColor = '#fff';
 var blackColor = 'black';
+var yellowColor = 'yellow';
 
 document.getElementById('grayButton').onclick = switchGray;
 document.getElementById('whiteButton').onclick = switchWhite;
+document.getElementById('yellowButton').onclick = switchYellow;
 
 function switchGray() {
   document.body.style.backgroundColor = grayColor;
- document.body.style.color = whiteColor;
+  document.body.style.color = whiteColor;
 }
 
 function switchWhite() {
   document.body.style.backgroundColor = whiteColor;
+  document.body.style.color = blackColor;
+}
+
+function switchYellow() {
+  document.body.style.backgroundColor = yellowColor;
   document.body.style.color = blackColor;
 }
