0c89553fe2c0de2c0e4a59efe6aede6cccb21501
terrieyoung <tyoung@jbsinternational.com>
2015-10-22 15:37:37 -0400

    assignment
    finalized javascript for the class 10 assignment

---
+++ class 10/10_Intro_jQuery/starter_code/color_scheme_switcher/js/index.js
@@ -0,0 +1,5 @@
+//Select your buttons and make them listen for click events. When someone clicks on them they run the color changing functions
+
+
+
+//Color changing functions to swap out the class attribute on the body (<body class="gray">). This lets you control the look and feel in your CSS.
