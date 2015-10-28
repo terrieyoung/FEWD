4226d3af0579ff01998b7e258de4735813bd99e8
terrieyoung <tyoung@jbsinternational.com>
2015-10-28 18:20:28 -0400

    final project
    fixed jewelry & books margin below images

---
+++ finalproject/js/index.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+
+
+$(function(){
+    $('.fadein img:gt(0)').hide();
+    setInterval(function(){
+      $('.fadein :first-child').fadeOut()
+         .next('img').fadeIn()
+         .end().appendTo('.fadein');}, 
+      4000);
+});
+
+
+
+
+});
