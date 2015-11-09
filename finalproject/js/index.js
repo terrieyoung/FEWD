813f4ce7b457b4944382f67e608be67a1ba057df
terrieyoung <tyoung@jbsinternational.com>
2015-11-09 18:18:50 -0500

    Work on Final project
    added back to top button, worked on carousel images, replaced photos,
    and icons.

--- finalproject/js/index.js
+++ finalproject/js/index.js
@@ -14,10 +14,20 @@ $(function(){
     $('.fadein2 img:gt(0)').hide();
     setInterval(function(){
       $('.fadein2 :first-child').fadeOut()
-         .next('img').fadeIn2()
+         .next('img').fadeIn()
          .end().appendTo('.fadein2');}, 
       4000);
 });
 
-
+$(window).scroll(function(){ 
+    if ($(this).scrollTop() > 100) { 
+        $('#scroll').fadeIn(); 
+    } else { 
+        $('#scroll').fadeOut(); 
+    } 
+}); 
+    $('#scroll').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+  }); 
 });
