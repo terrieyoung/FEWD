94355fe6a28c04b5d1469ed26fec5091e1d597a9
terrieyoung <tyoung@jbsinternational.com>
2015-11-18 20:08:50 -0500

    Changes to Final Project
    Made final changes, added anchor links, fixed logos and servcies
    alignment. Ready to present. May want to look at nav when sized down to
    mobile

--- finalproject/js/index.js
+++ finalproject/js/index.js
@@ -29,5 +29,20 @@ $(window).scroll(function(){
     $('#scroll').click(function(){ 
         $("html, body").animate({ scrollTop: 0 }, 600); 
         return false; 
-  }); 
+  });
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+}); 
 });
